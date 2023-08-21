c28682d7a23f19ed24d56ee8eee05b760a10f1e6
XeicuLy <106128913+XeicuLy@users.noreply.github.com>
2023-08-21 22:18:37 +0900

    ✨ : サムネイルがない時の処理追加

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import Link from 'next/link';
 
 import { getList } from '@/libs/microcms';
@@ -12,24 +13,28 @@ const Home = async () => {
       {data.contents.map((article) => (
         <li key={article.id} className={styles.list}>
           <Link href={`/articles/${article.id}`} className={styles.link}>
-            <picture>
-              <source
-                type='image/webp'
-                media='(max-width: 640px)'
-                srcSet={`${article.thumbnail?.url}?fm=webp&w=414 1x, ${article.thumbnail?.url}?fm=webp&w=414&dpr=2 2x`}
-              />
-              <source
-                type='image/webp'
-                srcSet={`${article.thumbnail?.url}?fm=webp&fit=crop&w=240&h=126 1x, ${article.thumbnail?.url}?fm=webp&fit=crop&w=240&h=126&dpr=2 2x`}
-              />
-              <img
-                src={article.thumbnail?.url}
-                alt=''
-                className={styles.image}
-                width={article.thumbnail?.width}
-                height={article.thumbnail?.height}
-              />
-            </picture>
+            {article.thumbnail ? (
+              <picture>
+                <source
+                  type='image/webp'
+                  media='(max-width: 640px)'
+                  srcSet={`${article.thumbnail?.url}?fm=webp&w=414 1x, ${article.thumbnail?.url}?fm=webp&w=414&dpr=2 2x`}
+                />
+                <source
+                  type='image/webp'
+                  srcSet={`${article.thumbnail?.url}?fm=webp&fit=crop&w=240&h=126 1x, ${article.thumbnail?.url}?fm=webp&fit=crop&w=240&h=126&dpr=2 2x`}
+                />
+                <img
+                  src={article.thumbnail?.url || `/no-image.png`}
+                  alt=''
+                  className={styles.image}
+                  width={article.thumbnail?.width}
+                  height={article.thumbnail?.height}
+                />
+              </picture>
+            ) : (
+              <Image className={styles.image} src='/no-image.png' alt='No Image' width={1200} height={630} />
+            )}
             <dl className={styles.content}>
               <dt className={styles.title}>{article.title}</dt>
               <dd>{formatDate(article.publishedAt || article.createdAt)}</dd>
