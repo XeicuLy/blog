fc2f7ffdbe14be152b54b30d84bfd7a85b86c69c
XeicuLy <106128913+XeicuLy@users.noreply.github.com>
2023-08-22 20:50:28 +0900

    ✨ : トップページ追加

---
+++ app/page.tsx
@@ -0,0 +1,18 @@
+import ArticleList from '@/components/ArticleList';
+import Pagination from '@/components/Pagination';
+import { LIMIT } from '@/constants';
+import { getList } from '@/libs/microcms';
+
+export const revalidate = 0;
+
+export default async function Page() {
+  const data = await getList({
+    limit: LIMIT,
+  });
+  return (
+    <>
+      <ArticleList articles={data.contents} />
+      <Pagination totalCount={data.totalCount} />
+    </>
+  );
+}
