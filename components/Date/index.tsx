a25c2533d3427028cf873fa892fd1928b294db54
XeicuLy <106128913+XeicuLy@users.noreply.github.com>
2023-08-23 00:08:11 +0900

    💄 : padding調整

--- components/Date/index.tsx
+++ components/Date/index.tsx
@@ -8,7 +8,7 @@ type Props = {
 
 export default function PublishedDate({ date }: Props) {
   return (
-    <span className='mx-0 my-3 flex items-center gap-2 leading-4'>
+    <span className='mx-0 my-3 flex items-center gap-2 pl-2 leading-4'>
       <Image src='/clock.svg' alt='' width={16} height={16} priority />
       {formatDate(date)}
     </span>
